1560e0b9a11e8fa21305d7dbafe3c928e7f4e879
tarik <tarik.ibnchekh@gmail.com>
2021-07-07 16:49:25 +0100

    ana amaji

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/steyle.css">
+</head>
+<body>
+<!-- navbar start  -->
+<nav class="navbar navbar-expand-sm navbar-light bg-light  ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"> school academy</a>
+    <span class="navbar-text"> bonjour</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse   offset-md-2 " id="navbarNav" >
+      <ul class="navbar-nav  custom-nav ">
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link "  href="#">Home</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">cours</a>
+        </li>
+        <li class="nav-item  custom-nav-item">
+          <a class="nav-link" href="#">payment</a>
+        </li>
+        <li class="nav-item  custom-nav-item">
+        <a class="nav-link" href="#">my profile</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">logout</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">login </a>
+        </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">sigup </a>
+        </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">feedback</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+        <a class="nav-link" href="#">contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<!-- end navbar -->
+ <!-- star le milieau -->
+ <div class="container-fluid">
+<div class="row">
+    <div class="col col-lg-6 ">
+      <div class="contenu">
+      <h1 >bienvenu dans la classe</h1>
+        <small>apprendre et mettre en œuvre</small> <br>
+        <a href="#" class="btn btn-danger">Commencer</a>
+      </div>
+
+
+    </div>
+    <div class="col col-lg-6">
+    <img class ="image" src="./image/up.png" alt="ss">
+    </div>
+
+
+</div>
+</div>
+
+
+ <!-- end melieu -->
+
+ <div class="container-fluid bg-danger" >
+ <div class="row footer">
+            <div class="col-sm ">
+            <h5 >  <i class="fas fa-book-open"></i> 100+ COURS EN LIGNE</h5>
+
+            </div>
+            <div class="col-sm ">
+        <h5 ><i class="fas fa-users"></i>  instructeurs experts</h5>
+            </div>
+            <div class="col-sm ">
+        <h5 > <i class="far fa-keyboard"></i> accès à vie</h5>
+            </div>
+            <div class="col-sm ">
+        <h5 > <i class="fas fa-wallet"></i> garantie de remboursement</h5>
+            </div>
+
+
+</div>
+
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/all.min.js"></script>
+
+</body>
+</html>
